0cecfeff2c5f03a309ad212c7fed0be89f23a1e4
Kathy Coffee <klcoffee8807@gmail.com>
2021-10-12 21:58:01 -0400

    Added constants for Tasks

---
+++ task.js
@@ -0,0 +1,13 @@
+// TASK MANAGER
+
+// Message to be used throughout the file/project
+const title = `TASK MANAGER`
+// Enter option prompt
+const enterOption = `What would you like to do? (Please enter one of the options below):`;
+// Options
+const op1 = `TASKS - Display All Tasks`;
+const op2 = `NEW - Add a New Task`;
+const op3 = `REMOVE - Remove a Task`;
+const op4 = `CLOSE - Close the Task Manager`
+// Closing salutation
+const exitMsg = `Thank You for Using the TASK MANAGER`
