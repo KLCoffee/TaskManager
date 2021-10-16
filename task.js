007fea84ea77f8f1a941f4bbf438dde284933e53
Kathy Coffee <klcoffee8807@gmail.com>
2021-10-15 22:18:53 -0400

    Changed landing page using two arrays

--- task.js
+++ task.js
@@ -1,39 +1,64 @@
 // TASK MANAGER
-// Message to be used throughout the file/project
-const enterOption = `TASK MANAGER
-What would you like to do? (Please enter one of the options below):
 
+// For starting & restarting Task Manager
+let enterTask = true
+// Message for main Task Manager Page
+const taskStart = [`TASK MANAGER
+
+What would you like to do? (Please enter one of the options below):`]
+
+// For viewing all tasks
+let allPrompts = [
+`
 "TASKS" - Display All Tasks
 "NEW" - Add a New Task
 "REMOVE" - Remove a Task
 "CLOSE" - Close Task Manager
 `
-// To close Task Manager
-const closeTask = "CLOSE"
-let userEntry = alert(`Thank you for using Task Manager`)
-// For adding to the tasks list
-let addTasks;
+]
+let allTasks = [
+      `Charge MacBook`,
+      `Learn JavaScript`
+]
 
-// For storing new tasks
-let newTasks;
+      
 
-// For removing tasks
-let removeTasks;
+      ;
+//                   }
 
-// Starting alert message
-prompt(enterOption);
+//       break;
+// }
 
-// Tasks may be viewed or added as long as newTasks has value of true
-while (addTasks){
-      if (closeTask.toUpperCase === userEntry);
+// // // // To close Task Manager
+// // const closeTask = "CLOSE"
+// // let userEntry = alert(`Thank you for using Task Manager`)
+// // // For adding to the tasks list
+// // let addTasks;
+
+// let userEntry;
+// if (userEntry.toUpperCase === close){
+//             alert(closeTask);
+
+while (true){
+      if(userEntry.toUpperCase()===`CLOSE`){
+            alert(`Thank you for using Task Manager`)
+      }
+      
+      
+      
+      break;}
+
+      
 
 
-break;}
 
-// For storing user's response to add task or remove task
-let moreTasks;
 
-// For displaying task list
-let allTasks;
+// while (addTasks){
+      
+
+
+// break;}
+
+
 
 
