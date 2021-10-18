06e5e9dc32279b6bf21029d8245892d5af56d222
Kathy Coffee <klcoffee8807@gmail.com>
2021-10-18 01:09:45 -0400

    Added prompts and alerts  for Tasks, New, Remove and Close. No connections between each

--- task.js
+++ task.js
@@ -1,64 +1,38 @@
 // TASK MANAGER
 
-// For starting & restarting Task Manager
-let enterTask = true
-// Message for main Task Manager Page
-const taskStart = [`TASK MANAGER
+let toDos = true;
+let userEntry;
+let taskEntry;
+      
+userEntry = prompt(`TASK MANAGER
 
-What would you like to do? (Please enter one of the options below):`]
-
-// For viewing all tasks
-let allPrompts = [
-`
+What would you like to do (Please enter one of the options below):
 "TASKS" - Display All Tasks
-"NEW" - Add a New Task
-"REMOVE" - Remove a Task
-"CLOSE" - Close Task Manager
+"NEW" - Add A New Task
+"REMOVE" - Remove A Task
+"CLOSE" - Close The Task Manageer
 `
-]
-let allTasks = [
-      `Charge MacBook`,
-      `Learn JavaScript`
-]
-
+);
+while (toDos){
+     while (true) {
+           if  (userEntry.toUpperCase() === `CLOSE`){
+            alert(`Thank you for using Task Manager.`);
+            // toDos = false;
+            break;
+       }
+     
+      else if (userEntry.toUpperCase() === `TASKS`)
+            alert(`Charge MacBook`,`Master JavaScript`);
       
+      else if (userEntry.toUpperCase() === `NEW`)
+            prompt(`Enter your new task below.`);
 
-      ;
-//                   }
-
-//       break;
-// }
-
-// // // // To close Task Manager
-// // const closeTask = "CLOSE"
-// // let userEntry = alert(`Thank you for using Task Manager`)
-// // // For adding to the tasks list
-// // let addTasks;
-
-// let userEntry;
-// if (userEntry.toUpperCase === close){
-//             alert(closeTask);
-
-while (true){
-      if(userEntry.toUpperCase()===`CLOSE`){
-            alert(`Thank you for using Task Manager`)
-      }
-      
-      
-      
-      break;}
-
-      
+      else  (userEntry.toUpperCase() === `REMOVE`)
+            prompt(`Which task to you wish to remove?`);
+            break;      
+      }    
+              break;
+}
 
 
-
-
-// while (addTasks){
-      
-
-
-// break;}
-
-
-
-
+  
